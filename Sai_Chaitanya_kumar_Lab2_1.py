69ab2a9078f69efbf75e3904b0473a5ff10e6318
Chaitanya2279 <90093672+Chaitanya2279@users.noreply.github.com>
2021-09-17 17:38:48 +0400

    Lab2

---
+++ Sai_Chaitanya_kumar_Lab2_1.py
@@ -0,0 +1,11 @@
+import turtle
+def test_drive():
+    turtle.pensize(5)
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.down()
+    turtle.goto(50,50)
+    turtle.home()
+    turtle.circle(25)
+test_drive()
